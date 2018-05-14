6b7aae3ba172b9ff20e1d6ee84be3eb8444009ea
ashish1904 <bunny.ashish.bhagam@gmail.com>
2018-05-14 18:49:15 -0400

    first commit

---
+++ booleans.py
@@ -0,0 +1,13 @@
+miles = input('how far would you like to travel:' )
+miles = int(miles)
+
+if miles < 3:
+    print ('I suggest you to walk')
+elif (miles > 3 and miles < 300):
+    print ('I suggest you to drive')
+else:
+    print ('I suggest you to fly')
+        
+
+            
+            
